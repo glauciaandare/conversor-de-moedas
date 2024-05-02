af9b854c3e2c343351fc15f74fe32c67f42823e2
Gláucia Cecília Pivoto Andare <glaucia.andare@gmail.com>
2024-05-01 21:58:42 -0300

    Criando Menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Conversor de Moedas");
-        System.out.println("----Menu----");
+        System.out.println("***************************************");
+        System.out.println("Seja bem-vindo/a ao Conversor de Moeda");
+        System.out.println();
+        System.out.println();
+        System.out.println("1) Dolar =>> Peso Argentino");
+        System.out.println("2) Peso Argentino =>> Dolar");
+
     }
 }
