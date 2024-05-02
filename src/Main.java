3df1edc20cc8071c760087f49e81bcc619659078
Gláucia Cecília Pivoto Andare <glaucia.andare@gmail.com>
2024-05-01 22:08:12 -0300

    Criando Menu

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("1) Dolar =>> Peso Argentino");
         System.out.println("2) Peso Argentino =>> Dolar");
         System.out.println("3) Dolar =>> Real Brasileiro");
+        System.out.println(") Real Brasileiro =>> Dolar");
 
     }
 }
